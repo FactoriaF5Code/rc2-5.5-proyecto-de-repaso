48151c9237e18ede735e841407c8ead7f86ea8ca
Javi <javi@jsrois.net>
2024-04-23 10:03:40 +0200

    Disable npm test watch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/tests/setup.js"
-  }
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/tests/setup.js",
+        watch: false
+    }
 })
