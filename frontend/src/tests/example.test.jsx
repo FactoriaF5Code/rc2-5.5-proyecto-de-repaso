1b243376b18c4ac9cc6864079402c515eef9f40c
Javi <javi@jsrois.net>
2024-04-23 10:00:05 +0200

    Add frontend testing

---
+++ frontend/src/tests/example.test.jsx
@@ -0,0 +1,12 @@
+import {describe, it, expect} from "vitest";
+import {render, screen} from "@testing-library/react";
+import App from "../App.jsx";
+
+describe("La aplicación", () => {
+    render(<App/>);
+
+    it(" muestra un mensaje ", () => {
+        expect(screen.getByText(/En construcción/)).toBeInTheDocument()
+    })
+})
+
