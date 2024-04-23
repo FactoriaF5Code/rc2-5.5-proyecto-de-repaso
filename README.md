808f174ec6c1b02d8c6306fda4ac29e59f91eb03
Javi <javi@jsrois.net>
2024-04-23 09:02:48 +0200

    Add failing test and badge

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Bootcamp App
 
+![build status](https://github.com/FactoriaF5Code/rc2-5.5-proyecto-de-repaso/actions/workflows/maven-build.yml/badge.svg)
+
+
 ## Hecho con React + JS, Springboot + Java
 
 
--- src/test/java/net/jsrois/bootcampapp/BootcampAppApplicationTests.java
+++ src/test/java/net/jsrois/bootcampapp/BootcampAppApplicationTests.java
@@ -3,12 +3,15 @@ package net.jsrois.bootcampapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 @SpringBootTest
 class BootcampAppApplicationTests {
 
     @Test
     void contextLoads() {
-
+        assertThat(2+2, equalTo(5));
     }
 
 }
