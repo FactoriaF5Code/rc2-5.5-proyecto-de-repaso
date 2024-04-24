e1112967e93d6cb302022fbac33a1ad0e455ab71
Javi <javi@jsrois.net>
2024-04-24 10:04:58 +0200

    Disable test

--- src/test/java/net/jsrois/bootcampapp/BootcampAppApplicationTests.java
+++ src/test/java/net/jsrois/bootcampapp/BootcampAppApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
 class BootcampAppApplicationTests {
 
     @Test
